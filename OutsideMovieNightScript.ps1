b35564304fd029268633bc422db805c7d094825a
Schwoby <65581397+Schwoby@users.noreply.github.com>
2024-10-10 00:10:03 -0500

    Update and rename OutsideMovieNightScript.py to OutsideMovieNightScript.ps1
    corrected file extension, consolidated redundant code, removed some old commented out code.

--- OutsideMovieNightScript.ps1
+++ OutsideMovieNightScript.ps1
@@ -2,30 +2,17 @@ $MovieName1 = 'ERNEST SCARED STUPID.mp4'
 $MovieName2 = 'FREE GUY.mp4'
 $MovieTimeOffSet = 150
 $DoubleFeatureIntermission = 600
-$filePath = 'C:\Users\Kurt Schwob\Videos\Movies\'
 
 
 
 
 
-#Get-NetAdapter
-#Enable-NetAdapter -Name 'Wi-Fi' -Confirm:$false -AsJob | Wait-Job
-#Echo ""
-#Echo "Turning Wi-Fi on"
-#while ((Get-NetAdapter -Name 'Wi-Fi' | where MediaConnectionState -eq 'Connected') -eq $null) {
-#	Start-Sleep -Seconds 1
-#	Echo "Connecting..."
-#}
-#Echo "Wi-Fi turned on"
-#Start-Sleep -Seconds 5
 curl.exe -d GET http://192.168.144.13:8123/api/webhook/BackyardMovieNightStart
-$fullPath1 = $filePath + $MovieName1
-$shell1 = New-Object -COMObject shell.Application
-$folder1 = Split-Path $fullPath1
-$file1 = Split-Path $fullPath1 -Leaf
-$shellfolder1 = $shell1.Namespace($folder1)
-$shellfile1 = $shellfolder1.ParseName($file1)
-$MovieTime1 = $shellfolder1.GetDetailsOf($shellfile1, 27);
+$filePath = Join-Path (Get-Location).Path 'Movies'
+$shell = New-Object -COMObject shell.Application
+$shellfolder = $shell.Namespace($filePath)
+$fullPath1 = Join-Path $filePath $MovieName1
+$MovieTime1 = $shellfolder.GetDetailsOf($shellfolder.ParseName($MovieName1), 27)
 $MovieTimeOffSet1 = $MovieTimeOffSet
 $MovieTimeDelay1 = [TimeSpan]::Parse($MovieTime1).TotalSeconds - $MovieTimeOffSet1
 if ( $MovieTimeDelay1 -lt $MovieTimeOffSet1 ) {
@@ -39,17 +26,13 @@ Start-Sleep -Seconds $MovieTimeOffSet1
 if ( $MovieName2 -ne '' ) {
 	Stop-Process -Name "mpc-hc64"
 	Start-Sleep -Seconds 1
-	Start-Process 'C:\Users\Kurt Schwob\Videos\AfterParty.mp4' -WindowStyle maximized ; Start-Sleep -seconds 3 ; [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') ; [System.Windows.Forms.SendKeys]::SendWait("{F11}")
+	$AfterParty = Join-Path (Get-Location).Path 'AfterParty.mp4'
+	Start-Process $AfterParty -WindowStyle maximized ; Start-Sleep -seconds 3 ; [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') ; [System.Windows.Forms.SendKeys]::SendWait("{F11}")
 	Start-Sleep -Seconds 15
 	Stop-Process -Name "mpc-hc64"
 	Start-Process C:\Windows\System32\MarineAquarium3.scr
-	$fullPath2 = $filePath + $MovieName2
-	$shell2 = New-Object -COMObject shell.Application
-	$folder2 = Split-Path $fullPath2
-	$file2 = Split-Path $fullPath2 -Leaf
-	$shellfolder2 = $shell2.Namespace($folder2)
-	$shellfile2 = $shellfolder2.ParseName($file2)
-	$MovieTime2 = $shellfolder2.GetDetailsOf($shellfile2, 27); 
+	$fullPath2 = Join-Path $filePath $MovieName2
+	$MovieTime2 = $shellfolder.GetDetailsOf($shellfolder.ParseName($MovieName2), 27)
 	$MovieTimeOffSet2 = $MovieTimeOffSet
 	$MovieTimeDelay2 = [TimeSpan]::Parse($MovieTime2).TotalSeconds - $MovieTimeOffSet2
 	if ( $MovieTimeDelay2 -lt $MovieTimeOffSet2 ) {
